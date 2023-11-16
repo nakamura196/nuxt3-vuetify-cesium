3f8a3c576159e42211ade0b00b5f57a3e5616385
Satoru <you@example.com>
2023-11-16 15:14:42 +0900

    feat: initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: [
+    "vuetify/lib/styles/main.sass"
+  ],
+  ssr: false,
+  build: {
+    transpile: ["vuetify"],
+  }
+});
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <vc-config-provider :cesium-path="vcConfig.cesiumPath">
+      <vc-viewer>
+        <vc-entity
+          :position="[139.762222, 35.713333]"
+          description="The University of Tokyo"
+        >
+          <vc-graphics-point
+            ref="point1"
+            color="red"
+            :pixel-size="8"
+          ></vc-graphics-point>
+        </vc-entity>
+      </vc-viewer>
+    </vc-config-provider>
+  </v-container>
+</template>
+<script lang="ts" setup>
+import {
+  VcConfigProvider,
+  VcViewer,
+  VcEntity,
+  VcGraphicsPoint
+} from "vue-cesium";
+
+const vcConfig = reactive({
+    cesiumPath: "https://unpkg.com/cesium@latest/Build/Cesium/Cesium.js",
+  });
+</script>
